debc055ae426710e2d1b254a8885934ec8e72a98
hj <dv.zinke@gmail.com>
2020-10-06 23:29:31 +0900

    feat : 공 컬러 추가

--- lib/config/lotto.dart
+++ lib/config/lotto.dart
@@ -4,4 +4,11 @@ class Lotto {
 
   static const Color mainColor = Color(0xFF009a8d);
 
+  static const Color numColor1 = Color(0xFFFBC400);
+  static const Color numColor2 = Color(0xFF69C8F2);
+
+  static const Color numColor3 = Color(0xFFFF7272);
+  static const Color numColor4 = Color(0xFFAAAAAA);
+  static const Color numColor5 = Color(0xFFB0D840);
+
 }

9df3fbb597faa1435865ac0cfcb7baab18b2bd28
hj <dv.zinke@gmail.com>
2020-10-07 23:50:13 +0900

    feat : 로또번호 위젯 생성

--- lib/screens/my_lotto_screen.dart
+++ lib/screens/my_lotto_screen.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:lotto_flutter/widgets/lotto_number_set.dart';
 import 'package:lotto_flutter/widgets/number_ball.dart';
 
 class MyLottoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: NumberBall(number: 5,)),
+      body: Center(child: LottoNumberSet(lottoNumbers: [1,11,21,31,33,45], bonusNumber: 1,)),
     );;
   }
 }
---
+++ lib/widgets/lotto_number_set.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:lotto_flutter/widgets/number_ball.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class LottoNumberSet extends StatelessWidget {
+  final List<int> lottoNumbers;
+  final int bonusNumber;
+  const LottoNumberSet({
+    Key key,
+    @required this.lottoNumbers,
+    this.bonusNumber
+  }) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      NumberBall(number: lottoNumbers[0],),
+      NumberBall(number: lottoNumbers[1],),
+      NumberBall(number: lottoNumbers[2],),
+      NumberBall(number: lottoNumbers[3],),
+      NumberBall(number: lottoNumbers[4],),
+      NumberBall(number: lottoNumbers[5],),
+      bonusNumber == null ?  Container() : Icon(Icons.add),
+      bonusNumber == null ?  Container() : NumberBall(number: lottoNumbers[5],),
+
+    ],);
+  }
+}
+
+List<Widget> getNumberWidget(List<int> lottoNumbers) {
+  List<Widget> lottoNumberWidgets = [];
+  for(int i=0; i<lottoNumbers.length; i++ ) {
+      lottoNumberWidgets.add(NumberBall(number: lottoNumbers[i],));
+  }
+
+  return lottoNumberWidgets;
+}

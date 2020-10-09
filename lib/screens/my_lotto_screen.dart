0bd31969debaefb99a898f20a659367933b52b51
hj <dv.zinke@gmail.com>
2020-10-09 23:04:21 +0900

    feat : my lotto screen header 추가

--- lib/screens/my_lotto_screen.dart
+++ lib/screens/my_lotto_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:lotto_flutter/config/lotto.dart';
 import 'package:lotto_flutter/widgets/lotto_number_set.dart';
 import 'package:lotto_flutter/widgets/number_ball.dart';
 
 class MyLottoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: LottoNumberSet(lottoNumbers: [1,11,21,31,33,45], bonusNumber: 1,)),
-    );;
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          brightness: Brightness.light,
+          backgroundColor: Lotto.mainColor,
+          title: Text(
+            '부자 로또',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                letterSpacing: -1.2),
+          ),
+          centerTitle: false,
+          floating: true,
+        ),
+        SliverToBoxAdapter(
+          child: LottoNumberSet(lottoNumbers: [1,11,21,31,41,45],),
+        )
+      ],
+    );
   }
 }
